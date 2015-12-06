811d3a4ada90c8753020b997f00d79a09b1013e2
Anton Larionov <larionov.nt@gmail.com>
2015-12-06 18:47:47 +0400

    playerctl: show mediaplayer info via mpris

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ shellcheck -s sh battery_upower/battery_upower
 shellcheck -s sh keyboard_xkb/keyboard_xkb
 shellcheck -s sh volume_pactl/volume_pactl
 shellcheck -s sh skype/skype
-
+shellcheck -s sh playerctl/playerctl
