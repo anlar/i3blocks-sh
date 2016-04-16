9c1e0369810ce24d036c2019548126130a84e122
Anton Larionov <larionov.nt@gmail.com>
2016-04-16 22:41:05 +0400

    Simplify test.sh: check all exec files

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
-shellcheck -s sh battery_acpi/battery_acpi
-shellcheck -s sh battery_upower/battery_upower
-shellcheck -s sh keyboard_xkb/keyboard_xkb
-shellcheck -s sh volume_pactl/volume_pactl
-shellcheck -s sh skype/skype
-shellcheck -s sh playerctl/playerctl
-shellcheck -s sh openvpn/openvpn
-shellcheck -s sh disk_free/disk_free
+#! /usr/bin/env sh
+
+find . -maxdepth 2 -mindepth 2 -type f -executable | xargs shellcheck -s sh
