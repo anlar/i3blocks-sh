363b734a4c90eef226d093fcc528367a0923f19b
Anton Larionov <larionov@haulmont.com>
2018-12-31 16:46:58 +0400

    Simplify volume script

--- blocks/volume.sh
+++ blocks/volume.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env sh
 
-if [ -n "$BLOCK_INSTANCE" ]; then
-  sink_name="$BLOCK_INSTANCE"
-else
-  sink_name=$(pactl list sinks short | head -1 | awk '{print $2}' |sed 's/.*\.//')
-fi
-
-level='5%'
+instance=$(pactl list sinks short | head -1 | awk '{print $2}' |sed 's/.*\.//')
+step='5%'
 no_sound='Off'
 
-while getopts s:l:n: opt; do
+if [ -n "$BLOCK_INSTANCE" ]; then
+  instance="$BLOCK_INSTANCE"
+fi
+
+while getopts t:u:q opt; do
   case "$opt" in
-    s) sink_name="$OPTARG" ;;
-    l) level="$OPTARG" ;;
     n) no_sound="$OPTARG" ;;
+    s) step="$OPTARG" ;;
   esac
 done
 
-
-sink=$(pactl list sinks short | grep "$sink_name" | awk '{print $1}')
+sink=$(pactl list sinks short | grep "$instance" | awk '{print $1}')
 mute=$(pactl list sinks | grep "Sink #$sink" -A 999999 | grep Mute | head -n1 | awk '{print $2}')
 
 if [ "$mute" = "yes" ] ; then
@@ -27,13 +24,11 @@ else
   volume="$(pactl list sinks | grep "Sink #$sink" -A 999999 | grep -P '\tVolume' | grep -P '\d+(?=%)' -o | head -1)%"
 fi
 
-
 case "$BLOCK_BUTTON" in
-  1|4) pactl set-sink-volume "$sink" +"$level" ;;
-  3|5) pactl set-sink-volume "$sink" -- -"$level" ;;
+  1|4) pactl set-sink-volume "$sink" +"$step" ;;
+  3|5) pactl set-sink-volume "$sink" -- -"$step" ;;
   2) pactl set-sink-mute "$sink" toggle ;;
 esac
-
 
 printf "%s\n" "$volume"
 printf "\n"
