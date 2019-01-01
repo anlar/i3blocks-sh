91304b80c6c4dc2ea98e1a26bfc4d98dd337c6d9
Anton Larionov <larionov@haulmont.com>
2019-01-01 15:36:27 +0400

    Fix deadbeef buttons

--- blocks/deadbeef.sh
+++ blocks/deadbeef.sh
@@ -37,9 +37,9 @@ else
 fi
 
 case "$BLOCK_BUTTON" in
-  1|4) deadbeef.sh --next ;;
-  3|5) deadbeef.sh --prev ;;
-  2) deadbeef.sh --play-pause ;;
+  1|4) "$instance" --next ;;
+  3|5) "$instance" --prev ;;
+  2) "$instance" --play-pause ;;
 esac
 
 if [ "$hide_inactive" -eq 1 ] && [ "$isactive" -eq 0 ]; then
@@ -48,4 +48,3 @@ fi
 
 printf "%s %s\n" "$status" "$song"
 printf "%s\n" "$song"
-
