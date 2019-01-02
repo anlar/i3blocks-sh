64fc924a1bda35e391fbc34e6ace1d603ffe47f8
Anton Larionov <larionov.nt@gmail.com>
2019-01-02 19:42:21 +0400

    Add lenght limit to deadbeef script

--- blocks/deadbeef.sh
+++ blocks/deadbeef.sh
@@ -4,23 +4,25 @@ instance=deadbeef
 playing=Play
 paused=Pause
 stopped=Stop
+max_length=1000
 hide_inactive=0
 
 if [ -n "$BLOCK_INSTANCE" ]; then
   instance="$BLOCK_INSTANCE"
 fi
 
-while getopts a:b:c:q opt; do
+while getopts a:b:c:m:q opt; do
   case "$opt" in
     a) playing="$OPTARG" ;;
     b) paused="$OPTARG" ;;
     c) stopped="$OPTARG" ;;
+    m) max_length="$OPTARG" ;;
     q) hide_inactive=1 ;;
     *) exit 1 ;;
   esac
 done
 
-title="$($instance --nowplaying-tf "%artist% - %title%")"
+title="$($instance --nowplaying-tf "%artist% - %title%" | cut -c -$max_length)"
 short_title="$($instance --nowplaying-tf "%title%")"
 isplaying="$($instance --nowplaying-tf "%isplaying%")"
 ispaused="$($instance --nowplaying-tf "%ispaused%")"
